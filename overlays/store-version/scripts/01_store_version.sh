c33a8375a6cb2f58458c08ff3b07255d21aa6b08
paxx12 <245230251+paxx12@users.noreply.github.com>
2025-12-10 12:59:53 +0100

    Revert "Store version in a firmware"
    This reverts commit eacdd7fd4e697abf0e566a0cd68e9bc8c008da9d.

--- overlays/store-version/scripts/01_store_version.sh
+++ overlays/store-version/scripts/01_store_version.sh
@@ -11,6 +11,5 @@ if [[ -n "$GIT_VERSION" ]]; then
   ABBRV=$(git describe --abbrev --always)
 
   # 0.9.0-paxx12-1-gabcdef0
-  echo "${GIT_VERSION#v}" > "$ROOTFS_DIR/etc/VERSION"
-  echo "${GIT_VERSION#v}_$(date +%Y%m%d%H%M%S)_${ABBRV}" > "$ROOTFS_DIR/etc/CUSTOMVERSION"
+  echo "${GIT_VERSION#v}-${ABBRV}" > "$ROOTFS_DIR/etc/CUSTOM_VERSION"
 fi
