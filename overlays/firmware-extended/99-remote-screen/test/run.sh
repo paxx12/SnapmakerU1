bdf2400b4c39f2715eef94652ecd8dcd6c914502
paxx12 <245230251+paxx12@users.noreply.github.com>
2025-12-29 09:47:20 +0100

    Add test script for remote screen development
    Provides a helper script to deploy and run fb-http-server
    on a device for testing during development.

---
+++ overlays/firmware-extended/99-remote-screen/test/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROOT_DIR="$(dirname "$(realpath "$0")")/.."
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 <u1.home> [fb-http-server-args...]"
+    exit 1
+fi
+
+HOST="$1"
+shift
+
+scp -r "$ROOT_DIR/root/." "root@$HOST:/"
+ssh -t "root@$HOST" /etc/init.d/S99fb-http stop
+ssh -t "root@$HOST" /usr/local/bin/fb-http-server.py --html "/usr/local/share/fb-http-server/index.html" "$@"
