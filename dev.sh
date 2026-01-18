f6390a1bcf4071c857d425ce8c86d74b58492d46
Scott Lemmon <skot.lemmon@gmail.com>
2026-01-18 11:55:04 -0500

    Add SSH key embedding and dev.sh SSH mounting for development
    - Add overlays/common/04-ssh-authorized-keys/ overlay for embedding SSH public keys
    - Add SSH folder mounting to dev.sh for container development
    - Update .gitignore to exclude personal SSH keys
    - Update upgrade-firmware.sh to remove the ssh host from known hosts, as it will fail due to the firmware upgrade changing the host sha

--- dev.sh
+++ dev.sh
@@ -4,6 +4,7 @@ set -e
 
 IMAGE_NAME="snapmaker-u1-dev"
 BUILD_CONTEXT=".github/dev"
+SSH_FOLDER="$HOME/.ssh"
 
 if ! docker build -t "$IMAGE_NAME" "$BUILD_CONTEXT"; then
     echo "[!] Docker build failed."
@@ -15,4 +16,4 @@ TTY_FLAG=""
 
 ENV_FLAGS="-e GIT_VERSION"
 
-exec docker run --rm $TTY_FLAG $ENV_FLAGS -w "$PWD" -v "$PWD:$PWD" "$IMAGE_NAME" "$@"
+exec docker run --rm $TTY_FLAG $ENV_FLAGS -w "$PWD" -v "$PWD:$PWD" -v "$SSH_FOLDER:/root/.ssh" "$IMAGE_NAME" "$@"
